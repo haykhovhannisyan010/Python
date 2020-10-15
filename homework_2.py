cc39ba2bd12055f2dca260f39d8a02c342c19875
haykhovhannisyan010 <72920366+haykhovhannisyan010@users.noreply.github.com>
2020-10-15 21:32:30 +0400

    Update homework_2.py

--- homework_2.py
+++ homework_2.py
@@ -1,3 +1,4 @@
+#Ruben
 #1 
 n = input()
 nums = []
@@ -11,7 +12,28 @@ for m in nums:
     else:
         d[int(m)] = nums.count(m)
 print(d)
+
+#2
+word = input()
+c = 0
+a = 0
+elements = {c:word.count(c) for c in set(word)}
+for e in elements.values():
+    if e % 2 == 0:
+        c += 1
+    else:
+        c -= 1
+        a += 1
+if c > 0:
+    even = True
+else:
+    even = False
+if even == True or (len(word) % 2 == 1) and a  == 1:
+    print(True)
+else:
+    print(False)
     
+#Narek 
 #1
 for i in range(100):
     for j in range(100):
@@ -23,4 +45,4 @@ for i in range(100):
 for row in range(1,5):
     for i in range(1,row+1):
         print(row, end=" ")  
-    print(" ")
+    #print(" ")
