7ed39a1603d73328b937783646bce651e9e6853b
haykhovhannisyan010 <haykhovhannisyan010@gmail.com>
2020-10-23 21:56:50 +0400

    homework 4

--- homework_3.py
+++ homework_3.py
@@ -1,28 +1,63 @@
 #Narek
-##1
-#def asc_order(a):
-    #c = 0
-    #for i in range(len(a) - 1):
-        #if a[i] >= a[i+1]:
-            #c += a[i] - a[i + 1] + 1 
-            #a[i+1] = a[i] + 1
-    #return c
-#print(asc_order([1,1,1]))
+#1
+def asc_order(a):
+    c = 0
+    for i in range(len(a) - 1):
+        if a[i] >= a[i+1]:
+            c += a[i] - a[i + 1] + 1 
+            a[i+1] = a[i] + 1
+    return c
+print(asc_order([1,1,1]))
 
-##2
-#def al(a):
-    #c = 0
-    #for i in range(len(a) - 1):
-        #if a[i] > a[i + 1]:
-            #c +=1      
-    #return c <= 1
-#print(al([5,4,6,9,3]))
+#2
+def al(a):
+    a.insert(0,min(a)-1)
+    c = 0
+    for i in range(2,len(a)):
+        if a[i] <= a[i - 1] and a[i] > a[i - 2]:
+            c += 1
+            a[i-1] = a[i]
+        elif a[i] <= a[i - 1] and a[i] <= a[i -2]:
+            c += 1
+            a[i]=a[i-1]
+    return c <= 1 
+print(al([1,2,1,2]))
 
 #Ruben
 #1
-n = int(input())
-a = [[input(),float(input())] for i in range(n)]
+a = [[input(),float(input())] for _ in range(int(input()))]
 second = sorted(list(set([elem[1] for elem in a])))[1]
 b = [el[0] for el in a if second == el[1]]
 for elem in sorted(b):
     print(elem)
+
+#2
+def describe_the_str(string):
+    dict_ = {}
+    for symb in set(string):
+        dict_[symb] = string.count(symb)
+    return dict_
+
+def dict_contains(dict_1,dict_2):
+    for key in dict_2:
+        if dict_1.get(key,0) < dict_2[key]:
+            return False
+    return True
+def min_len_sub(superString,subString):
+    super_length = len(superString)
+    sub_length = len(subString)
+    desc_of_subString = describe_the_str(subString)
+    t = 0
+    while sub_length + t <= super_length :
+        start,end = 0 ,sub_length + t
+        while end <= super_length and not dict_contains(describe_the_str(superString[start:end]),desc_of_subString):
+            start += 1
+            end += 1
+        if dict_contains(describe_the_str(superString[start:end]),desc_of_subString):
+            return (superString[start:end])
+        elif end > super_length:
+            t+= 1
+    return "The SuperString doesn't contain the Substring"       
+superString = input("Input the superstring : ")
+subString = input("Input the substring : ")
+print(min_len_sub(superString,subString))
---
+++ homework_4.py
@@ -0,0 +1,65 @@
+#Narek
+#1
+a = [2,8,5,3,4,6,7]
+minn = b = 0
+c = []
+for j in range(2,max(a)): 
+    for i in a:
+        if i % j != 0:
+            minn+= 1
+            if minn == len(a):
+                b = j
+                c.append(b)
+        else:
+            minn = 0
+print(min(c) if c else max(a)+1)
+               
+#2
+n = "2x+15-1+2=35"
+c = 0
+for i in range(len(n)):
+    c += 1
+    if n[i] == "=":
+        break
+a = n[:c-1]
+b = [n[c:]]
+h = [n[c:]]
+x = []
+for j in range(len(a)):
+    if not "x" in a[j]:
+        if a[j] == "+":
+            b.append("-")
+        elif a[j] == "-":
+            b.append("+")
+        else:
+            b.append(a[j])
+    else:
+        while h != b:
+            if b[-1] != "+" or b[-1] != "-":
+                x.append(b[-1])
+                b.pop()
+                break
+b = "".join(b)
+x.reverse()
+x = "".join(x)
+if x:
+    print(eval(b) / int(x[0]))
+else:
+    print(eval(b))
+    
+    
+#Ruben
+#1
+string = input()
+vowel={}
+consonant={}
+l = []
+for i in range(len(string)):
+    if string[i] in "aeoiu":
+        l.append(i)
+        b = string
+        for j in b[i+1:]:
+            print(b + j)
+            b += j
+            
+print(l)
