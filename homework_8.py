f760af480cfdd929afbe83687dc039d00e96dc33
haykhovhannisyan010 <72920366+haykhovhannisyan010@users.noreply.github.com>
2020-11-06 17:32:50 +0400

    Update homework_8.py

--- homework_8.py
+++ homework_8.py
@@ -40,27 +40,3 @@ def non_decreasing_sequence(*nums):
                     l[j] = -l[j]
         return 'No'
 print(non_decreasing_sequence(7,4,3,-2))
-
-
-
-#def non_decreasing_sequence(*nums):
-    #list1 = list(nums)
-    #for i in range(len(list1)):
-        #list1[i] = 0 - list1[i]
-        #if list1 == sorted(list1):
-            #return 'Yes'
-        #else:
-            #list1[i] = 0 - list1[i]
-    #for i in range(len(list1)):
-        #for j in range(len(list1)):
-            #list1[i] = 0 - list1[i]
-            #list1[j] = 0 - list1[j]
-            #if list1 == sorted(list1):
-                #return 'Yes'
-            #else:
-                #list1[i] = 0 - list1[i]
-                #list1[j] = 0 - list1[j]
-    #return 'No'
-
-
-#print(non_decreasing_sequence(7,4,3,2))
