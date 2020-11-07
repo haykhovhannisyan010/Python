3f57b24e0940f165e7c536918cebc8878e07d174
haykhovhannisyan010 <72920366+haykhovhannisyan010@users.noreply.github.com>
2020-11-07 11:44:00 +0400

    Update homework_8.py

--- homework_8.py
+++ homework_8.py
@@ -34,36 +34,43 @@ print(l_m([9, 5, 8, 5, 20, 1, 2, -20, -1, -30]))
 #Ruben 
 #1
 def answer_queries(k, *q):
-    q = list(q)
     c = 1
-    if len(q) < 2:
-        if k < q[0]:
-            return (q[0] // k + 1)
-    else:
-        if k > q[0]:
-            return 1
+    l = list(q)
+    for i in range(len(l)):
+        if i == len(l) - 1:
+            if k - l[i] > 0:
+                return count
+            elif k < l[i]:
+                c += (l[i]) // k
+                return c            
         else:
-            return(sum(q) // k + 1)
-        
-print(answer_queries(3,100))
+            if k - l[i] > 0:
+                return c
+            elif k < l[i]:
+                l[i + 1] += l[i] - k
+            c += 1                    
+    return c + 1
+
+
+print(answer_queries(5,6,3,3,3))
+
 
 #2
 def non_decreasing_sequence(*nums):
     l = list(nums)
-    if l == sorted(l,reverse = True):
-        return "Yes"
-    else:
-        for i in range(len(l)):
-            for j in range(len(l)):
-                l[i] = -l[i]
-                l[j] = -l[j]
-                if l == sorted(l):
-                    return 'Yes'
-                else:
-                    l[i] = -l[i]
-                    l[j] = -l[j]
-        return 'No'
-print(non_decreasing_sequence(1,1,1))
+    for i in range(1, len(l)):
+        if abs(l[i-1]) < abs(l[i]):
+            if l[i] < 0:
+                l[i] = abs(l[i])            
+        else:
+            if l[i-1] >= 0:
+                l[i-1] = -l[i-1]            
+    if l == sorted(l):
+        return 'Yes', l
+    return 'No'
+
+print(non_decreasing_sequence(7,2,5,8))
+
 <<<<<<< HEAD
 
 =======
