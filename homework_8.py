767a142d2a0250a7287ff18f0bcb295f402468d2
haykhovhannisyan010 <haykhovhannisyan010@gmail.com>
2020-11-09 16:24:39 +0400

    homework

--- homework_8.py
+++ homework_8.py
@@ -16,44 +16,61 @@ get_books_and_date("John", Book_1 =["John",1984], Book_2 =["Bob",1974],\
     
 #2
 def l_m(l):
-    l.sort(reverse = True)
-    return l[0] * l[1] * l[2]
-print(l_m([9, 5, 8, 5, 20, 1, 2, -3, -2, -1, 0]))
+    l = [9, 5, 8, 5, 20, 1, -20, -1, -30] 
+    positive = [i for i in l if i > 0]
+    negative = [j for j in l if j < 0]
+    positive.sort(reverse = True)
+    negative.sort()
+    if len(negative) > 1:
+        if abs(negative[0]) * abs(negative[1]) * positive[0] > positive[0] \
+        * positive[1] * positive[2]:
+            return (abs(negative[0]) * abs(negative[1]) * positive[0])
+        else:
+            return (positive[0]  * positive[1] * positive[2]) 
+    else:
+        return (positive[0]  * positive[1] * positive[2]) 
+print(l_m([9, 5, 8, 5, 20, 1, 2, -20, -1, -30]))
 
 #Ruben 
 #1
 def answer_queries(k, *q):
-    q = list(q)
     c = 1
-    if len(q) < 2:
-        if k < q[0]:
-            return (q[0] // k + 1)
-    else:
-        if k > q[0]:
-            return 1
+    l = list(q)
+    for i in range(len(l)):
+        if i == len(l) - 1:
+            if k - l[i] > 0:
+                return c
+            elif k < l[i]:
+                c += (l[i]) // k
+                return c            
         else:
-            return(sum(q) // k + 1)
-        
-print(answer_queries(3,100))
-
+            if k - l[i] > 0:
+                return c
+            elif k < l[i]:
+                l[i + 1] += l[i] - k
+            c += 1                    
+    return c + 1
+print(answer_queries(5,6,3,3,3))
 #2
+import math
 def non_decreasing_sequence(*nums):
-    l = list(nums)
-    if l == sorted(l,reverse = True):
-        return "Yes"
-    else:
-        for i in range(len(l)):
-            for j in range(len(l)):
-                l[i] = -l[i]
-                l[j] = -l[j]
-                if l == sorted(l):
-                    return 'Yes'
-                else:
-                    l[i] = -l[i]
-                    l[j] = -l[j]
-        return 'No'
-print(non_decreasing_sequence(1,1,1))
-<<<<<<< HEAD
-
-=======
->>>>>>> 22d6c0956a2eaaf4382f3d8fa899d98e4659b85c
+    nums = list(nums)
+    i = 0
+    while i < len(nums) - 1:
+        if nums[i] == nums[i + 1]:
+            prev = nums[i - 1] if i - 1 >= 0 else -math.inf
+            next = nums[i + 2] if i + 2 < len(nums) else math.inf
+            if prev <= nums[i] >= next and prev <= -nums[i] <= next:
+                nums[i], nums[i + 1] = -nums[i], -nums[i + 1]
+        elif abs(nums[i]) == abs(nums[i + 1]):
+            if -nums[i + 1] >= nums[i + 1]:
+                nums[i + 1] = -nums[i + 1]
+        elif nums[i] > nums[i + 1] and abs(nums[i]) > abs(nums[i + 1]):
+            nums[i], nums[i + 1] = -nums[i], -nums[i + 1]
+        elif nums[i] > nums[i + 1] and abs(nums[i]) < abs(nums[i + 1]):
+            nums[i + 1] = -nums[i + 1]
+        i += 1
+    if nums == sorted(nums):
+        return f"Yes \n{nums}"
+    return "No"
+print(non_decreasing_sequence(3,2,5))
---
+++ homework_9.py
@@ -0,0 +1,50 @@
+#Ruben
+#1
+def buildPalindrome(string):
+    b = string[::-1]
+    for i in range(len(b),0,-1):
+        c = b[i:]
+        if string + c == c[::-1] + string[::-1]:
+            return (string + c)
+print(buildPalindrome("abcd"))
+
+#2
+a = set()  
+x = 0   
+def all_increasing_sequences(k,n,x,l):   
+    global a
+    if x == n:
+        for i in range(n): 
+            a.add((l[0],l[1],l[2]))
+    else:
+        i = 1 if x == 0 else l[x - 1] + 1
+        x += 1    
+        while (i <= k): 
+            l[x - 1] = i
+            all_increasing_sequences(k,n,x,l)
+            i += 1
+        x -= 1  
+        
+def all_increasing_sequences_print(n,k): 
+    l = [0] * n
+    all_increasing_sequences(k, n, x, l)
+    return a
+        
+print(all_increasing_sequences_print(3,5))
+
+#Narek
+#1
+def get_coin(l):
+    a = l
+    if l == [1]:
+        return b.index(l[0])
+    else:
+        l = len(l) // 2
+        if 1 in a[l:]: 
+            return get_coin(a[l:])
+        else:
+            return get_coin(a[:l])
+b = [2,2,2,1,2,2,2,2]      
+print(get_coin(b))
+
+#2
