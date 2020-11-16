08a8db36b37a64195f25282f12fd3acde9059ccf
haykhovhannisyan010 <haykhovhannisyan010@gmail.com>
2020-11-16 22:10:23 +0400

    homework

--- homework_10.py
+++ homework_10.py
@@ -10,19 +10,27 @@ def absolute(l):
 print(absolute([5,9,2,12,5,8]))
 
 #2
-def c(n):
-    c = 0
-    a = []
-    for i in range(n+1):
-        a.append(str(i))
-    a = "".join(a)
-    for i in range(len(a)):
-        if a[i] == "2":
-            c += 1
-    return c 
+def count_of2s_in_range(n):
+    count=0
+    n_string=str(n)
+    curent=int(n_string[0])
+    if int(n_string[0])>2:
+        count+=10**(len(n_string)-1)
+    elif int(n_string[0])==2:
+        count+=n-(2*(10**(len(n_string)-1)))+1
+    for i in range(1,len(n_string)):
+        if int(n_string[i]) > 2:
+            count += (10 ** (len(n_string) - 1-i))*(curent+1)
+            curent=int(n_string[0:i+1])
+        elif int(n_string[i]) < 2:
+            count += (10 ** (len(n_string) - 1-i) * curent)
+            curent = int(n_string[0:i + 1])
+        else:
+            count += (10 ** (len(n_string) - 1-i) * curent+(int(n_string[i:])-(2*(10**(len(n_string)-1-i)))+1))
+            curent = int(n_string[0:i + 1])
+    return count
+print(count_of2s_in_range(22))
         
-    
-print(c(22))
     
         
     
---
+++ homework_11.py
@@ -0,0 +1,74 @@
+#Narek
+#1
+def countDigit(n):
+    count = 0
+    while n != 0:
+        n //= 10
+        count += 1
+    return count
+print(countDigit(24))
+
+#2
+def sum_elements(ints, summ):
+    s = set()
+    a = []
+    for elem in ints:
+        if summ - elem in s:
+            a.append(tuple([summ - elem, elem]))
+        s.add(elem)
+    return a if a else  -1
+print(sum_elements([5,2,2,1,3],4))
+
+
+#Ruben
+#1
+def rotateImage(a):
+    for i in range(len(a) // 2):
+        end = len(a) - 1 - i
+        for j in range(end - i):
+            k = a[i][i+j]
+            a[i][i+j] = a[end-j][i]
+            a[end-j][i] = a[end][end-j]
+            a[end][end-j] = a[i+j][end]
+            a[i+j][end] = k
+    return a
+
+
+print(rotateImage([[10,9,6,3,7],
+                   [6,10,2,9,7],
+                   [7,6,3,8,2],
+                   [8,9,7,9,9],
+                   [6,8,6,8,2]]))
+
+
+#2
+a = []
+def get_digits(product):
+    global a
+    if product in range(9,1,-1):
+        a.append(product)
+    else:
+        for i in range(9,1,-1):
+            if product%i == 0:
+                a.append(i)
+                break
+            if i == 2 and product % i != 0:
+                a.append(-1)
+                return -1
+        return get_digits(product//i)       
+    
+def digitsProduct(product):
+    if product < 10: return product
+    elif product == 0: return 10    
+    get_digits(product)
+    if -1 in a:
+        return -1
+    str_number= ''
+    for i in reversed(a):
+        str_number+= str(i)
+    return int(str_number)
+ 
+
+print(digitsProduct(16))
+
+
