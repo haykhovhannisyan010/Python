162d376d1b0fae2eb678c50e41171bfaf3a110df
haykhovhannisyan010 <72920366+haykhovhannisyan010@users.noreply.github.com>
2020-11-21 09:20:52 +0400

    Update homework_12.py

--- homework_12.py
+++ homework_12.py
@@ -17,12 +17,11 @@ numstriangle(9)
 
 #2
 def luckynum(a):
-    for i in range(len(a)):
-        if i == a[i]:
-            return a[i]
+    for elem in enumerate(a):
+        if elem[0] == elem[1]:
+            return elem[0]
     return -1
-        
-print(luckynum([0,1,2,5,7,8]))
+print(luckynum([-1,0,1,4]))
 
 
 
